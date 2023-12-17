8f801b41f1eff6e1aa4860a32117232be8dd7f76
mr_sd_jack_003 <barson0habra@gmail.com>
2023-12-18 03:33:22 +0530

    done with server and client side and half frontend

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -5,7 +5,25 @@ class SocketService{
 
     constructor(){
         console.log("Init Socket Service.....");
-        this._io = new Server();
+        this._io = new Server({
+            cors: {
+                allowedHeaders: ["*"],
+                origin: "*",
+            },
+        });
+    }
+
+    public initListener(){
+        const io = this._io;
+        console.log("Initializing Socket Listeners.....");
+
+        io.on('connect', async socket => {
+            console.log(`New Socket Connected`, socket.id);
+
+            socket.on('event:message', async ({message}: {message: string}) => {
+                console.log("Ne message recieved.", message);
+            });
+        });
     }
 
     get io(){
